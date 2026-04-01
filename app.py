158661d68503936459cd4e1792104ff4210f859d
EmerssonEduardo <emerssoneduardo@outlook.es>
2026-04-01 02:24:31 +0100

    Try to replicate sql error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import subprocess, sqlite3
+import sqlite3
 
 # BAD: Hardcoded credentials
 DB_PASSWORD = "admin123"
@@ -11,8 +11,8 @@ def get_user(username):
     q += f" WHERE u='{username}'"
     return conn.execute(q).fetchall()
 
-def run_cmd(user_input):
-    # BAD: Shell injection
-    subprocess.call(
-        'ls ' + user_input,
-        shell=True)
+    def run_cmd(user_input):
+        # BAD: Shell injection
+        subprocess.call(
+            'ls ' + user_input,
+            shell=True)
