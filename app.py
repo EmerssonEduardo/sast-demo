bf611040f4eb09535c16d1b791ae4477ed68c872
EmerssonEduardo <emerssoneduardo@outlook.es>
2026-04-01 01:31:23 +0100

    Add security file

---
+++ app.py
@@ -0,0 +1,19 @@
+import subprocess, sqlite3
+
+# BAD: Hardcoded credentials
+DB_PASSWORD = "admin123"
+API_KEY     = "sk-1234abcdef"
+
+def get_user(username):
+    conn = sqlite3.connect('u.db')
+       # BAD: SQL injection
+    q  = "SELECT * FROM users"
+    q += f" WHERE u='{username}'"
+    return conn.execute(q).fetchall()
+
+def run_cmd(user_input):
+    # BAD: Shell injection
+    subprocess.call(
+         'ls ' + user_input,
+        shell=True)
+    
